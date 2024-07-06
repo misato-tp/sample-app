6d21b84282e8a74688635ca3db08ed698428aa06
misato-tp <tommy21miko15@gmail.com>
2024-07-07 00:09:57 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get  "/help",    to: "statics_pages#help"
   get  "/about",   to: "statics_pages#about"
   get  "/contact", to: "statics_pages#contact"
+  get  "/signup",   to: "users#new"
 end
